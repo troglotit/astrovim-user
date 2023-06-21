a68ff0957669c80ed013cd059eaf77d4b0622cdd
Alexander Argunov <alexander.argunov@tern.com>
2023-06-21 12:04:07 +0300

    updates

--- init.lua
+++ init.lua
@@ -61,7 +61,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true, -- enable or disable format on save globally
+        enabled = false, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
@@ -72,10 +72,13 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- "sumneko_lua",
       },
-      timeout_ms = 1000, -- default format timeout
-      -- filter = function(client) -- fully override the default formatting function
-      --   return true
-      -- end
+      timeout_ms = 5000, -- default format timeout
+      filter = function(client) -- fully override the default formatting function
+        -- print(client)
+        -- print(client.name)
+        return client.name == "null-ls"
+        -- return false
+      end
     },
     -- enable servers that you already have installed without mason
     servers = {
