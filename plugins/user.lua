6cebff1ff15e6fd617c5384d2df22549e8cee1c8
Alexander Argunov <alexander.argunov@tern.com>
2023-06-30 17:56:28 +0300

    add cmp-cmdline

--- plugins/user.lua
+++ plugins/user.lua
@@ -86,4 +86,37 @@ return {
       { "<leader>gn", "<cmd>Neogit<CR>", desc = "Open Neogit Tab Page" },
     },
   },
+  { -- override nvim-cmp plugin
+    "hrsh7th/nvim-cmp",
+    keys = { ":", "/", "?" }, -- lazy load cmp on more keys along with insert mode
+    dependencies = {
+      "hrsh7th/cmp-cmdline", -- add cmp-cmdline as dependency of cmp
+    },
+    config = function(plugin, opts)
+      local cmp = require "cmp"
+      -- run cmp setup
+      cmp.setup(opts)
+
+      -- configure `cmp-cmdline` as described in their repo: https://github.com/hrsh7th/cmp-cmdline#setup
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
+        }),
+      })
+    end,
+  },
 }
